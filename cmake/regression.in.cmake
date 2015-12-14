affa1ea113ddfd7a081fd765005a251979c1a1b3
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-12-14 12:59:58 +0000

    regression created via cmake functions
    Makes it easier to add more regressions.
    Also, the cmake script to run regressions is specialized for each test.
    This way we can run each test individually by simply calling the
    (executable) file in build/regressions

---
+++ cmake/regression.in.cmake
@@ -0,0 +1,42 @@
+#! @CMAKE_COMMAND@ -P
+# Sometimes HDF5 fails to manipulate a previously written file
+# Might be because OPTIMET, might be because HDF5
+set(output_blessed @regr_BLESSED@)
+set(test_outputs @regr_OUTPUTS@)
+set(diff_cmd @regr_DIFF_CMD@)
+
+if("${diff_cmd}" STREQUAL "")
+  set(diff_cmd ${CMAKE_COMMAND} -E compare_files)
+endif()
+
+foreach(output ${test_blessed})
+  if(EXISTS "${output}")
+    file(REMOVE "${output}")
+  endif()
+endforeach()
+
+execute_process(COMMAND @regr_CMD@ RESULT_VARIABLE test_not_successful)
+
+if( test_not_successful )
+  message( SEND_ERROR "@regr_CMD@ returned ${test_not_successful}" )
+endif( test_not_successful )
+
+list(LENGTH output_blessed len1)
+math(EXPR len2 "${len1} - 1")
+
+# diff_cmd contains the executable that will be invoked to compare the output
+# files
+foreach(index RANGE ${len2})
+  list(GET output_blessed ${index} blessed)
+  list(GET test_outputs ${index} test)
+  execute_process(
+    COMMAND ${diff_cmd};${blessed};${test}
+    RESULT_VARIABLE test_not_successful
+    OUTPUT_QUIET
+    ERROR_QUIET
+  )
+
+  if( test_not_successful )
+    message( SEND_ERROR "${test} does not match ${blessed}!")
+  endif( test_not_successful )
+endforeach()
