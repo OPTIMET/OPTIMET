cac6c9da03dbcfd6d33dec89202adc0878404723
Gary Macindoe <gary.macindoe@ucl.ac.uk>
2015-12-16 16:58:29 +0000

    More types.h -> Types.h

--- unittests/single_particle.cpp
+++ unittests/single_particle.cpp
@@ -1,6 +1,6 @@
 #include "catch.hpp"
 
-#include "types.h"
+#include "Types.h"
 #include "Geometry.h"
 #include "Scatterer.h"
 #include "constants.h"
@@ -20,7 +20,8 @@ TEST_CASE("Add scatterers to geometry") {
     CHECK(geometry.objects.front().vR.phi == Approx(0));
     CHECK(geometry.objects.front().elmag.epsilon_r.real() == Approx(1.1));
     CHECK(geometry.objects.front().elmag.epsilon_r.imag() == Approx(0));
-    CHECK(geometry.objects.front().elmag.epsilon.real() == Approx(1.1 * consEpsilon0));
+    CHECK(geometry.objects.front().elmag.epsilon.real() ==
+          Approx(1.1 * consEpsilon0));
     CHECK(geometry.objects.front().elmag.epsilon.imag() == Approx(0));
     CHECK(geometry.objects.front().elmag.mu_r.real() == Approx(1.2));
     CHECK(geometry.objects.front().elmag.mu_r.imag() == Approx(0));
@@ -28,7 +29,8 @@ TEST_CASE("Add scatterers to geometry") {
     CHECK(geometry.objects.front().elmag.mu.imag() == Approx(0));
     CHECK(geometry.objects.front().radius == Approx(0.5));
     CHECK(geometry.objects.front().nMax == 2);
-    CHECK(geometry.objects.front().sourceCoef.size() == 2*Tools::iteratorMax(2));
+    CHECK(geometry.objects.front().sourceCoef.size() ==
+          2 * Tools::iteratorMax(2));
   }
 
   SECTION("Add second scatterer") {
@@ -40,6 +42,7 @@ TEST_CASE("Add scatterers to geometry") {
       CHECK(geometry.objects.back().vR.phi == Approx(0.0));
     }
     SECTION("Overlap -- touching")
-      CHECK_THROWS_AS(geometry.pushObject({{2, 0, 0}, {1.1e0, 1.2e0}, 0.5, 2}), std::runtime_error);
+    CHECK_THROWS_AS(geometry.pushObject({{2, 0, 0}, {1.1e0, 1.2e0}, 0.5, 2}),
+                    std::runtime_error);
   }
 }
