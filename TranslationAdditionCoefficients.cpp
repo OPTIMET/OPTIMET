e00db1c23d5c0c2380c9caf8e031afa3e59450ca
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-01-05 14:50:31 +0000

    Using boost to compute factorials
    It knows how to avoid overflows, presumably.

--- TranslationAdditionCoefficients.cpp
+++ TranslationAdditionCoefficients.cpp
@@ -6,41 +6,50 @@
 #include <cmath>
 
 #include <boost/math/special_functions/legendre.hpp>
+#include <boost/math/special_functions/factorials.hpp>
 
 namespace optimet {
 namespace {
 //! True if n and m within shperical harmonics validity regime
-constexpr bool is_valid(t_int n, t_int m) { return n >= 0 and std::abs(m) <= n; }
+constexpr bool is_valid(t_int n, t_int m) {
+  return n >= 0 and std::abs(m) <= n;
+}
 //! True if both pairs are valid
 constexpr bool is_valid(t_int n, t_int m, t_int l, t_int k) {
   return is_valid(n, m) and is_valid(l, k);
 }
-//! Does just that
-constexpr t_int factorial(t_int n) { return n < 2 ? 1 : n * factorial(n - 1); }
+//! Eases computing ratios of two factorials
+inline t_real factorial_ratio(t_int n, t_int m) {
+  if (n == m)
+    return 1;
+  if (n > m)
+    return boost::math::falling_factorial<t_real>(n, n - m);
+  return 1e0 / boost::math::falling_factorial<t_real>(m, m - n);
+}
 //! Coefficient of Stout (2004) Appendix C recurrence relationship
 inline t_real a_plus(t_int n, t_int m) {
-  if(not is_valid(n, m))
+  if (not is_valid(n, m))
     return 0e0;
   return std::sqrt(static_cast<t_real>((n + m + 1) * (n - m + 1)) /
                    static_cast<t_real>((2 * n + 1) * (2 * n + 3)));
 }
 //! Coefficient of Stout (2004) Appendix C recurrence relationship
 inline t_real a_minus(t_int n, t_int m) {
-  if(not is_valid(n, m))
+  if (not is_valid(n, m))
     return 0e0;
   return std::sqrt(static_cast<t_real>((n + m) * (n - m)) /
                    static_cast<t_real>((2 * n + 1) * (2 * n - 1)));
 }
 //! Coefficient of Stout (2004) Appendix C recurrence relationship
 inline t_real b_plus(t_int n, t_int m) {
-  if(not is_valid(n, m))
+  if (not is_valid(n, m))
     return 0e0;
   return std::sqrt(static_cast<t_real>((n + m + 2) * (n + m + 1)) /
                    static_cast<t_real>((2 * n + 1) * (2 * n + 3)));
 }
 //! Coefficient of Stout (2004) Appendix C recurrence relationship
 inline t_real b_minus(t_int n, t_int m) {
-  if(not is_valid(n, m))
+  if (not is_valid(n, m))
     return 0e0;
   return std::sqrt(static_cast<t_real>((n - m) * (n - m - 1)) /
                    static_cast<t_real>((2 * n + 1) * (2 * n - 1)));
@@ -48,30 +57,31 @@ inline t_real b_minus(t_int n, t_int m) {
 } // anonymous namespace
 
 t_complex Ynm(Spherical<t_real> const &R, t_int n, t_int m) {
-  if(not is_valid(n, m))
+  if (not is_valid(n, m))
     return 0;
-  auto const gamma =
-      static_cast<t_real>(n * (n + 1) * (2 * n + 1)) /
-      (constant::pi * static_cast<t_real>(4 * n * (n + 1))) *
-      (static_cast<t_real>(factorial(n - m)) / static_cast<t_real>(factorial(n + m)));
+  auto const gamma = static_cast<t_real>(2 * n + 1) /
+                     (constant::pi * static_cast<t_real>(4)) *
+                     (factorial_ratio(n - m, n + m));
   return std::sqrt(gamma) * std::exp(constant::i * (m * R.phi)) *
          boost::math::legendre_p(n, m, std::cos(R.the));
 }
 
 namespace details {
 t_complex CachedRecurrence::operator()(t_int n, t_int m, t_int l, t_int k) {
-  // It simplifies the recurrence if we assume zero outside the domain of validity
-  if(not is_valid(n, m, l, k))
+  // It simplifies the recurrence if we assume zero outside the domain of
+  // validity
+  if (not is_valid(n, m, l, k))
     return 0e0;
 
-  // For simplicity, coefficients for negative m should be implemented separately using the symmetry
+  // For simplicity, coefficients for negative m should be implemented
+  // separately using the symmetry
   // relationship.
   assert(m >= 0);
 
   // Check cache first
   t_indices const indices{{n, m, k, l}};
   auto const i_found = cache.find(indices);
-  if(i_found != cache.end())
+  if (i_found != cache.end())
     return i_found->second;
 
   auto const result = recurrence(n, m, l, k);
@@ -80,22 +90,24 @@ t_complex CachedRecurrence::operator()(t_int n, t_int m, t_int l, t_int k) {
 }
 
 t_complex CachedRecurrence::recurrence(t_int n, t_int m, t_int l, t_int k) {
-  if(n == 0 and m == 0)
+  if (n == 0 and m == 0)
     return initial(l, k);
-  else if(n == m)
+  else if (n == m)
     return diagonal_recurrence(n, l, k);
   else
     return offdiagonal_recurrence(n, m, l, k);
 }
 
 t_complex CachedRecurrence::initial(t_int l, t_int k) {
-  if(l == 0 and k == 0)
-    return 1e0 / std::sqrt(4e0 * constant::pi);
   auto const wave = direction.rrr * waveK;
-  auto const bessel = regular ? optimet::bessel<Bessel> : optimet::bessel<Hankel1>;
+  auto const bessel =
+      regular ? optimet::bessel<Bessel> : optimet::bessel<Hankel1>;
   auto const hb = std::get<0>(bessel(wave, l + 1));
-  auto const factor = std::sqrt(4e0 * constant::pi) * ((l + k) % 2 == 0 ? 1 : -1);
-  assert(hb.size() > l + 1 and l >= 0);
+  if (l == 0 and k == 0)
+    return hb[l];
+  auto const factor =
+      std::sqrt(4e0 * constant::pi) * ((l + k) % 2 == 0 ? 1 : -1);
+  assert(hb.size() > l and l >= 0);
   return factor * Ynm(direction, l, -k) * hb[l];
 }
 
@@ -105,7 +117,8 @@ t_complex CachedRecurrence::diagonal_recurrence(t_int n, t_int l, t_int k) {
          b_plus(n - 1, n - 1);
 }
 
-t_complex CachedRecurrence::offdiagonal_recurrence(t_int n, t_int m, t_int l, t_int k) {
+t_complex CachedRecurrence::offdiagonal_recurrence(t_int n, t_int m, t_int l,
+                                                   t_int k) {
   return (-operator()(n - 2, m, l, k) * a_minus(n - 1, m) +
           operator()(n - 1, m, l - 1, k) * a_plus(l - 1, k) +
           operator()(n - 1, m, l + 1, k) * a_minus(l + 1, k)) /
@@ -113,11 +126,13 @@ t_complex CachedRecurrence::offdiagonal_recurrence(t_int n, t_int m, t_int l, t_
 }
 } // end of detailed namespace
 
-t_complex TranslationAdditionCoefficients::operator()(t_int n, t_int m, t_int l, t_int k) {
-  if(m >= 0)
+t_complex TranslationAdditionCoefficients::operator()(t_int n, t_int m, t_int l,
+                                                      t_int k) {
+  if (m >= 0)
     return positive(n, m, l, k);
 
-  auto const sign = negative.is_regular() ? ((k + l) % 2 == 0) : ((n + l + k + m) % 2 != 0);
+  auto const sign =
+      negative.is_regular() ? ((k + l) % 2 == 0) : ((n + l + k + m) % 2 != 0);
   auto const result = negative(n, -m, l, -k);
   return sign ? result : -result;
 }
--- unittests/translation_addition.cpp
+++ unittests/translation_addition.cpp
@@ -3,6 +3,7 @@
 #include "TranslationAdditionCoefficients.h"
 #include "constants.h"
 #include "Bessel.h"
+#include "AJ_AuxFuns.h"
 #include <boost/math/special_functions/legendre.hpp>
 
 using namespace optimet;
@@ -12,19 +13,29 @@ TEST_CASE("Check Ynm") {
   Spherical<t_real> const R(1e0, 0.42, 0.36);
   t_complex const waveK(1e0, 1.5e0);
 
-  auto const Y10 = std::sqrt(6e0 / 8e0 / constant::pi) * legendre_p(1, 0, std::cos(R.the));
-  auto const Y11 = std::sqrt(6e0 / 16e0 / constant::pi) * legendre_p(1, 1, std::cos(R.the)) *
+  auto const Y00 =
+      std::sqrt(1e0 / 4e0 / constant::pi) * legendre_p(0, 0, std::cos(R.the));
+  auto const Y10 =
+      std::sqrt(6e0 / 8e0 / constant::pi) * legendre_p(1, 0, std::cos(R.the));
+  auto const Y11 = std::sqrt(6e0 / 16e0 / constant::pi) *
+                   legendre_p(1, 1, std::cos(R.the)) *
                    std::exp(constant::i * R.phi);
-  auto const Y1m1 = std::sqrt(12e0 / 8e0 / constant::pi) * legendre_p(1, -1, std::cos(R.the)) *
+  auto const Y1m1 = std::sqrt(12e0 / 8e0 / constant::pi) *
+                    legendre_p(1, -1, std::cos(R.the)) *
                     std::exp(-constant::i * R.phi);
-  auto const Y2m1 = std::sqrt(180e0 / 24e0 / constant::pi) * legendre_p(2, -1, std::cos(R.the)) *
+  auto const Y2m1 = std::sqrt(180e0 / 24e0 / constant::pi) *
+                    legendre_p(2, -1, std::cos(R.the)) *
                     std::exp(-constant::i * R.phi);
-  auto const Y32 = std::sqrt(84e0 / 5760e0 / constant::pi) * legendre_p(3, 2, std::cos(R.the)) *
+  auto const Y32 = std::sqrt(84e0 / 5760e0 / constant::pi) *
+                   legendre_p(3, 2, std::cos(R.the)) *
                    std::exp(constant::i * 2e0 * R.phi);
-  auto const Y3m2 = std::sqrt(10080e0 / 48e0 / constant::pi) * legendre_p(3, -2, std::cos(R.the)) *
+  auto const Y3m2 = std::sqrt(10080e0 / 48e0 / constant::pi) *
+                    legendre_p(3, -2, std::cos(R.the)) *
                     std::exp(-constant::i * 2e0 * R.phi);
-  auto const Y40 = std::sqrt(4320e0 / 1920e0 / constant::pi) * legendre_p(4, 0, std::cos(R.the));
+  auto const Y40 = std::sqrt(4320e0 / 1920e0 / constant::pi) *
+                   legendre_p(4, 0, std::cos(R.the));
 
+  CHECK(std::abs(Y00 - Ynm(R, 0, 0)) == Approx(0));
   CHECK(std::abs(Y10 - Ynm(R, 1, 0)) == Approx(0));
   CHECK(std::abs(Y11 - Ynm(R, 1, 1)) == Approx(0));
   CHECK(std::abs(Y1m1 - Ynm(R, 1, -1)) == Approx(0));
@@ -35,7 +46,8 @@ TEST_CASE("Check Ynm") {
 }
 
 template <class RECURRENCE>
-void check_recurrence(Spherical<t_real> const &R, t_complex const &waveK, bool is_regular) {
+void check_recurrence(Spherical<t_real> const &R, t_complex const &waveK,
+                      bool is_regular) {
   using namespace boost::math;
   RECURRENCE ta(R, waveK, is_regular);
 
@@ -49,21 +61,25 @@ void check_recurrence(Spherical<t_real> const &R, t_complex const &waveK, bool i
   }
 
   SECTION("Check initial conditions") {
-    CHECK(ta(0, 0, 0, 0).real() == Approx(1e0 / std::sqrt(4e0 * constant::pi)));
-    CHECK(ta(0, 0, 0, 0).imag() == Approx(0));
-
     auto const bess = is_regular ? bessel<Bessel> : bessel<Hankel1>;
     auto const hb = std::get<0>(bess(R.rrr * waveK, 4));
-    auto const Y10 = std::sqrt(6e0 / 8e0 / constant::pi) * legendre_p(1, 0, std::cos(R.the));
-    auto const Y1m1 = std::sqrt(12e0 / 8e0 / constant::pi) * legendre_p(1, -1, std::cos(R.the)) *
+    auto const Y10 =
+        std::sqrt(6e0 / 8e0 / constant::pi) * legendre_p(1, 0, std::cos(R.the));
+    auto const Y1m1 = std::sqrt(12e0 / 8e0 / constant::pi) *
+                      legendre_p(1, -1, std::cos(R.the)) *
                       std::exp(-constant::i * R.phi);
     auto const Y3m2 = std::sqrt(10080e0 / 48e0 / constant::pi) *
-                      legendre_p(3, -2, std::cos(R.the)) * std::exp(-constant::i * 2e0 * R.phi);
-    auto const Y32 = std::sqrt(84e0 / 5760e0 / constant::pi) * legendre_p(3, 2, std::cos(R.the)) *
+                      legendre_p(3, -2, std::cos(R.the)) *
+                      std::exp(-constant::i * 2e0 * R.phi);
+    auto const Y32 = std::sqrt(84e0 / 5760e0 / constant::pi) *
+                     legendre_p(3, 2, std::cos(R.the)) *
                      std::exp(constant::i * 2e0 * R.phi);
-    auto const Y20 = std::sqrt(60e0 / 48e0 / constant::pi) * legendre_p(2, 0, std::cos(R.the));
-    auto const Y40 = std::sqrt(4320e0 / 1920e0 / constant::pi) * legendre_p(4, 0, std::cos(R.the));
+    auto const Y20 = std::sqrt(60e0 / 48e0 / constant::pi) *
+                     legendre_p(2, 0, std::cos(R.the));
+    auto const Y40 = std::sqrt(4320e0 / 1920e0 / constant::pi) *
+                     legendre_p(4, 0, std::cos(R.the));
     auto const factor = std::sqrt(4e0 * constant::pi);
+    CHECK(std::abs(ta(0, 0, 0, 0) - hb[0]) == Approx(0));
     CHECK(std::abs(ta(0, 0, 1, 0) + factor * Y10 * hb[1]) == Approx(0));
     CHECK(std::abs(ta(0, 0, 1, 1) - factor * Y1m1 * hb[1]) == Approx(0));
     CHECK(std::abs(ta(0, 0, 3, 2) + factor * Y3m2 * hb[3]) == Approx(0));
@@ -77,20 +93,23 @@ void check_recurrence(Spherical<t_real> const &R, t_complex const &waveK, bool i
     auto const a00_20 = ta(0, 0, 2, 0);
     auto const a00_40 = ta(0, 0, 4, 0);
     auto const left1 = std::sqrt(2e0 / 3e0) * a11_31;
-    auto const right1 = std::sqrt(12e0 / 35e0) * a00_20 + std::sqrt(12e0 / 63e0) * a00_40;
+    auto const right1 =
+        std::sqrt(12e0 / 35e0) * a00_20 + std::sqrt(12e0 / 63e0) * a00_40;
     CHECK(left1.real() == Approx(right1.real()));
     CHECK(left1.imag() == Approx(right1.imag()));
 
     auto const a11_51 = ta(1, 1, 5, 1);
     auto const a00_60 = ta(0, 0, 6, 0);
     auto const left2 = std::sqrt(2e0 / 3e0) * a11_51;
-    auto const right2 = std::sqrt(30e0 / 99e0) * a00_40 + std::sqrt(30e0 / 143e0) * a00_60;
+    auto const right2 =
+        std::sqrt(30e0 / 99e0) * a00_40 + std::sqrt(30e0 / 143e0) * a00_60;
     CHECK(left2.real() == Approx(right2.real()));
     CHECK(left2.imag() == Approx(right2.imag()));
 
     auto const a22_42 = ta(2, 2, 4, 2);
     auto const left3 = std::sqrt(12e0 / 15e0) * a22_42;
-    auto const right3 = std::sqrt(30e0 / 63e0) * a11_31 + std::sqrt(12e0 / 99e0) * a11_51;
+    auto const right3 =
+        std::sqrt(30e0 / 63e0) * a11_31 + std::sqrt(12e0 / 99e0) * a11_51;
     CHECK(left3.real() == Approx(right3.real()));
     CHECK(left3.imag() == Approx(right3.imag()));
 
@@ -98,7 +117,8 @@ void check_recurrence(Spherical<t_real> const &R, t_complex const &waveK, bool i
     auto const a00_31 = ta(0, 0, 3, 1);
     auto const a00_51 = ta(0, 0, 5, 1);
     auto const left0 = std::sqrt(2e0 / 3e0) * a11_42;
-    auto const right0 = std::sqrt(30e0 / 63e0) * a00_31 + std::sqrt(12e0 / 99e0) * a00_51;
+    auto const right0 =
+        std::sqrt(30e0 / 63e0) * a00_31 + std::sqrt(12e0 / 99e0) * a00_51;
     CHECK(left0.real() == Approx(right0.real()));
     CHECK(left0.imag() == Approx(right0.imag()));
   }
@@ -108,7 +128,8 @@ void check_recurrence(Spherical<t_real> const &R, t_complex const &waveK, bool i
     auto const a00_21 = ta(0, 0, 2, 1);
     auto const a00_41 = ta(0, 0, 4, 1);
     auto const left0 = std::sqrt(1e0 / 3e0) * a10_31;
-    auto const right0 = std::sqrt(8e0 / 35e0) * a00_21 + std::sqrt(15e0 / 63e0) * a00_41;
+    auto const right0 =
+        std::sqrt(8e0 / 35e0) * a00_21 + std::sqrt(15e0 / 63e0) * a00_41;
     CHECK(left0.real() == Approx(right0.real()));
     CHECK(left0.imag() == Approx(right0.imag()));
 
@@ -117,7 +138,8 @@ void check_recurrence(Spherical<t_real> const &R, t_complex const &waveK, bool i
     auto const a42_21 = ta(4, 2, 2, 1);
     auto const a42_41 = ta(4, 2, 4, 1);
     auto const left1 = std::sqrt(21e0 / 99e0) * a52_31;
-    auto const right1 = -std::sqrt(12e0 / 63e0) * a32_31 + std::sqrt(8e0 / 35e0) * a42_21 +
+    auto const right1 = -std::sqrt(12e0 / 63e0) * a32_31 +
+                        std::sqrt(8e0 / 35e0) * a42_21 +
                         std::sqrt(15e0 / 63e0) * a42_41;
     CHECK(left1.real() == Approx(right1.real()));
     CHECK(left1.imag() == Approx(right1.imag()));
@@ -128,7 +150,8 @@ void check_recurrence(Spherical<t_real> const &R, t_complex const &waveK, bool i
     auto const a32_00 = ta(3, 2, 0, 0);
     auto const a42_10 = ta(4, 2, 1, 0);
     auto const left0 = std::sqrt(21e0 / 99e0) * a52_00;
-    auto const right0 = -std::sqrt(12e0 / 63e0) * a32_00 + std::sqrt(1e0 / 3e0) * a42_10;
+    auto const right0 =
+        -std::sqrt(12e0 / 63e0) * a32_00 + std::sqrt(1e0 / 3e0) * a42_10;
     CHECK(left0.real() == Approx(right0.real()));
     CHECK(left0.imag() == Approx(right0.imag()));
   }
@@ -137,15 +160,23 @@ void check_recurrence(Spherical<t_real> const &R, t_complex const &waveK, bool i
 TEST_CASE("Translation-Addition positive m") {
   Spherical<t_real> const R(1e0, 0.42, 0.36);
   t_complex const waveK(1e0, 1.5e0);
-  SECTION("Regular") { check_recurrence<details::CachedRecurrence>(R, waveK, true); }
-  SECTION("Irregular") { check_recurrence<details::CachedRecurrence>(R, waveK, false); }
+  SECTION("Regular") {
+    check_recurrence<details::CachedRecurrence>(R, waveK, true);
+  }
+  SECTION("Irregular") {
+    check_recurrence<details::CachedRecurrence>(R, waveK, false);
+  }
 }
 
 TEST_CASE("Translation-Addition all m") {
   Spherical<t_real> const R(1e0, 0.42, 0.36);
   t_complex const waveK(1e0, 1.5e0);
-  SECTION("Regular") { check_recurrence<TranslationAdditionCoefficients>(R, waveK, true); }
-  SECTION("Irregular") { check_recurrence<TranslationAdditionCoefficients>(R, waveK, false); }
+  SECTION("Regular") {
+    check_recurrence<TranslationAdditionCoefficients>(R, waveK, true);
+  }
+  SECTION("Irregular") {
+    check_recurrence<TranslationAdditionCoefficients>(R, waveK, false);
+  }
   SECTION("Negative regular") {
     TranslationAdditionCoefficients ta(R, waveK, true);
     TranslationAdditionCoefficients ta_conj(R, std::conj(waveK), true);
@@ -178,4 +209,18 @@ TEST_CASE("Translation-Addition all m") {
     CHECK(ta(5, -3, 3, -1).real() == Approx(-ta_conj(5, 3, 3, 1).real()));
     CHECK(ta(5, -3, 3, -1).imag() == Approx(-ta_conj(5, 3, 3, 1).imag()));
   }
+}
+
+TEST_CASE("Regression of Ynm coefficients") {
+  Spherical<t_real> const R(1e0, 0.42, 0.36);
+  auto const nmax = 20;
+  auto const dataYp = optimet::compute_Yp(R, nmax);
+  for (t_int n(1), index(0); n < nmax; ++n) {
+    for (t_int m(n); m >= -n; --m, ++index) {
+      CHECK(dataYp[index].real() == Approx(optimet::Ynm(R, n, m).real()));
+      CHECK(dataYp[index].imag() == Approx(optimet::Ynm(R, n, m).imag()));
+    }
+  }
+  CHECK(dataYp.back().real() == Approx(optimet::Ynm(R, 0, 0).real()));
+  CHECK(dataYp.back().imag() == Approx(optimet::Ynm(R, 0, 0).imag()));
 }
