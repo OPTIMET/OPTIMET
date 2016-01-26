e3807cdc00ab975eb6d7cd0aa9244598e76513c7
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-01-26 11:20:56 +0000

    Tested more complex transfers

--- unittests/scalapack_matrix.cpp
+++ unittests/scalapack_matrix.cpp
@@ -29,7 +29,7 @@ TEST_CASE("Creates a matrix in 1x1 context") {
   CHECK(matrix.blacs()[5] == 10);
   CHECK(matrix.blacs()[6] == 0);
   CHECK(matrix.blacs()[7] == 0);
-  auto const leading = matrix.eigen().IsRowMajor ? matrix.eigen().cols(): matrix.eigen().rows();
+  auto const leading = matrix.eigen().IsRowMajor ? matrix.eigen().cols() : matrix.eigen().rows();
   CHECK(matrix.blacs()[8] == leading);
 }
 
@@ -42,18 +42,18 @@ TEST_CASE("Creates a matrix in 1x2 context") {
   CHECK(matrix.eigen().rows() == (context.is_valid() ? 64 : 0));
   CHECK(matrix.eigen().cols() == (context.is_valid() ? 32 : 0));
   CHECK(matrix.blacs()[0] == 1);
-  CHECK(matrix.blacs()[1] == (context.is_valid() ? *context: -1));
+  CHECK(matrix.blacs()[1] == (context.is_valid() ? *context : -1));
   CHECK(matrix.blacs()[2] == 64);
   CHECK(matrix.blacs()[3] == 64);
   CHECK(matrix.blacs()[4] == 16);
   CHECK(matrix.blacs()[5] == 8);
   CHECK(matrix.blacs()[6] == 0);
   CHECK(matrix.blacs()[7] == 0);
-  auto const leading = matrix.eigen().IsRowMajor ? matrix.eigen().cols(): matrix.eigen().rows();
+  auto const leading = matrix.eigen().IsRowMajor ? matrix.eigen().cols() : matrix.eigen().rows();
   CHECK(matrix.blacs()[8] == leading);
 }
 
-void check(t_uint n, t_uint m) {
+void check_creation(t_uint n, t_uint m) {
   if(scalapack::global_size() < n * m)
     return;
   auto const rank = scalapack::global_rank();
@@ -96,12 +96,12 @@ void check(t_uint n, t_uint m) {
 }
 
 TEST_CASE("Creates a matrice in nxm context") {
-  check(1, 1);
-  check(2, 1);
-  check(1, 2);
-  check(2, 2);
-  check(3, 1);
-  check(3, 2);
+  SECTION("1x1") { check_creation(1, 1); }
+  SECTION("2x1") { check_creation(2, 1); }
+  SECTION("1x2") { check_creation(1, 2); }
+  SECTION("2x2") { check_creation(2, 2); }
+  SECTION("3x1") { check_creation(3, 1); }
+  SECTION("3x2") { check_creation(3, 2); }
 }
 
 TEST_CASE("Transfer from 1x1 to 2x1") {
@@ -148,4 +148,49 @@ TEST_CASE("Transfer from 1x1 to 2x1") {
     // Check input matrix was recovered
     CHECK(input_matrix.isApprox(gathered.eigen(), 1e-12));
   }
+}
+
+void check_distribute(optimet::scalapack::Matrix::Sizes const &grid,
+                      optimet::scalapack::Matrix::Sizes const &size,
+                      optimet::scalapack::Matrix::Sizes const &blocks) {
+  mpi::Communicator const world;
+  if(world.size() < grid.rows * grid.cols) {
+    WARN("Not enough processes to run test: " << grid.rows << "x" << grid.cols << " < "
+                                              << world.size());
+    return;
+  }
+
+  auto const ranks = world.all_gather(scalapack::global_rank());
+  auto const root = std::find(ranks.begin(), ranks.end(), 0) - ranks.begin();
+  scalapack::Context const single(1, 1);
+  scalapack::Context const parallel(grid.rows, grid.cols);
+  scalapack::Context const transpose(grid.cols, grid.rows);
+  scalapack::Context const all(world.size(), 1);
+
+  auto const split = mpi::Communicator().split(single.is_valid() or parallel.is_valid());
+  scalapack::Matrix input(single, size, blocks);
+  if(single.is_valid())
+    input.eigen() = optimet::Matrix<t_real>::Random(size.rows, size.cols);
+  auto const input_matrix = split.broadcast(input.eigen(), root);
+
+  // perform transfer
+  auto scattered = input.transfer_to(all, parallel);
+  scattered.eigen() *= 2;
+  // go to transpose
+  auto transposed = scattered.transfer_to(transpose);
+  transposed.eigen() *= 1.5;
+  // return to single
+  auto const gathered = transposed.transfer_to(single);
+  // Global size remain the same
+  CHECK(gathered.rows() == input.rows());
+  CHECK(gathered.cols() == input.cols());
+  if(single.is_valid())
+    CHECK((3e0 * input_matrix).isApprox(gathered.eigen(), 1e-12));
+}
+
+TEST_CASE("Transfer from 1x1 to nxm to mxn to 1x1") {
+  SECTION("1x2") { check_distribute({1, 2}, {1024, 2048}, {32, 65}); }
+  SECTION("3x1") { check_distribute({3, 1}, {1024, 2048}, {32, 65}); }
+  SECTION("2x2") { check_distribute({2, 2}, {1024, 2048}, {32, 65}); }
+  SECTION("3x2") { check_distribute({3, 2}, {1024, 2048}, {32, 65}); }
 }
