f84d341bb513b495e68c92736f2c105d30647362
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-05-18 14:33:16 +0100

    More accurate and flexible typing for the linear systems

--- scalapack/LinearSystemSolver.h
+++ scalapack/LinearSystemSolver.h
@@ -23,19 +23,19 @@ namespace scalapack {
 template <class SCALAR> int general_linear_system_inplace(Matrix<SCALAR> &A, Matrix<SCALAR> &b);
 //! Solves a system of linear equations
 template <class SCALAR>
-std::tuple<Matrix<SCALAR>, int>
+std::tuple<typename Matrix<SCALAR>::ConcreteMatrix, int>
 general_linear_system(Matrix<SCALAR> const &A, Matrix<SCALAR> const &b);
 
 #ifdef OPTIMET_BELOS
 //! Solve a system of linear equations using Belos
-template <class SCALAR>
-std::tuple<Matrix<SCALAR>, int>
-gmres_linear_system(Matrix<SCALAR> const &A, Matrix<SCALAR> const &b,
+template <class SCALARA, class SCALARB>
+std::tuple<typename Matrix<SCALARA>::ConcreteMatrix, int>
+gmres_linear_system(Matrix<SCALARA> const &A, Matrix<SCALARB> const &b,
                     Teuchos::RCP<Teuchos::ParameterList> const &parameters,
                     mpi::Communicator const &comm = mpi::Communicator());
-template <class SCALAR>
-std::tuple<Matrix<SCALAR>, int>
-gmres_linear_system(Matrix<SCALAR> const &A, Matrix<SCALAR> const &b,
+template <class SCALARA, class SCALARB>
+std::tuple<typename Matrix<SCALARA>::ConcreteMatrix, int>
+gmres_linear_system(Matrix<SCALARA> const &A, Matrix<SCALARB> const &b,
                     mpi::Communicator const &comm = mpi::Communicator());
 #endif
 }
