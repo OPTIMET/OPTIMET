b2b424503f139918aed2ea9ecc043346f8e8bb66
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-06-20 09:38:02 +0100

    Add guards around some MPI-only functions

--- Solver.h
+++ Solver.h
@@ -180,6 +180,7 @@ Matrix<t_complex> preconditioned_scattering_matrix(Geometry const &geometry,
 Vector<t_complex> distributed_source_vector(Vector<t_complex> const &input,
                                             scalapack::Context const &context,
                                             scalapack::Sizes const &blocks);
+#ifdef OPTIMET_MPI
 //! Gather the distributed vector into a single vector
 Vector<t_complex> gather_all_source_vector(t_uint n, Vector<t_complex> const &input,
                                            scalapack::Context const &context,
@@ -187,7 +188,6 @@ Vector<t_complex> gather_all_source_vector(t_uint n, Vector<t_complex> const &in
 //! Gather the distributed vector into a single vector
 Vector<t_complex> gather_all_source_vector(scalapack::Matrix<t_complex> const &matrix);
 
-#ifdef OPTIMET_MPI
 //! \brief Broadcast data from a proc in the context to procs outside the context
 //! \details Usefull if some procs are not part of the context but still require the data.
 template <class T>
