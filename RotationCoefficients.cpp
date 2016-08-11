047daf6f57411700aed94ffeb9d7a9d31ee49879
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-08-11 14:48:03 +0100

    Adding overall types so we can change precision easily

--- RotationCoefficients.cpp
+++ RotationCoefficients.cpp
@@ -4,24 +4,23 @@
 #include <iterator>
 
 namespace optimet {
-namespace {
 
-t_real a(t_uint n, t_uint m) {
-  if(n < m)
+RotationCoefficients::Real RotationCoefficients::a(t_uint n, t_int m) {
+  t_uint const absm(std::abs(m));
+  if(n < absm)
     return 0;
-  return std::sqrt(static_cast<t_real>((n + 1 + m) * (n + 1 - m)) /
-                   static_cast<t_real>((2 * n + 1) * (2 * n + 3)));
+  return std::sqrt(static_cast<Real>((n + 1 + absm) * (n + 1 - absm)) /
+                   static_cast<Real>((2 * n + 1) * (2 * n + 3)));
 }
-t_real a(t_uint n, t_int m) { return a(n, static_cast<t_uint>(std::abs(m))); }
 
-t_real b(t_uint n, t_int m) {
+RotationCoefficients::Real RotationCoefficients::b(t_uint n, t_int m) {
   if(static_cast<t_uint>(std::abs(m)) > n)
     return 0;
-  return (m >= 0 ? 1 : -1) * std::sqrt(static_cast<t_real>((n - m - 1) * (n - m)) /
-                                       static_cast<t_real>((2 * n - 1) * (2 * n + 1)));
+  return (m >= 0 ? 1 : -1) * std::sqrt(static_cast<Real>((n - m - 1) * (n - m)) /
+                                       static_cast<Real>((2 * n - 1) * (2 * n + 1)));
 }
-}
-t_complex RotationCoefficients::operator()(t_uint n, t_int m, t_int mu) {
+
+RotationCoefficients::Complex RotationCoefficients::operator()(t_uint n, t_int m, t_int mu) {
   if(static_cast<t_uint>(std::abs(m)) > n or static_cast<t_uint>(std::abs(mu)) > n)
     return 0;
   if(m < 0)
@@ -36,8 +35,7 @@ t_complex RotationCoefficients::operator()(t_uint n, t_int m, t_int mu) {
   return value;
 }
 
-void RotationCoefficients::coefficients(t_uint n, t_int m, t_int mu,
-                                        std::vector<t_complex> &coeffs) {
+void RotationCoefficients::coefficients(t_uint n, t_int m, t_int mu, std::vector<Complex> &coeffs) {
 
   if(m == 0) {
     coeffs.push_back(initial(n, mu));
@@ -46,34 +44,34 @@ void RotationCoefficients::coefficients(t_uint n, t_int m, t_int mu,
   if(m < 0) {
     auto const conjgate = coefficients(n, -m, -mu);
     std::transform(conjgate.begin(), conjgate.end(), std::back_inserter(coeffs),
-                   [](t_complex const &c) { return std::conj(c); });
+                   [](Complex const &c) { return std::conj(c); });
     return;
   }
 
-  auto const factor = std::exp(t_complex(0, chi)) / b(n + 1, m - 1);
+  auto const factor = std::exp(Complex(0, chi)) / b(n + 1, m - 1);
   auto const c0 =
-      factor * 0.5 * b(n + 1, -mu - 1) * std::exp(t_complex(0, phi)) * (1 - std::cos(theta));
+      factor * 0.5 * b(n + 1, -mu - 1) * std::exp(Complex(0, phi)) * (1 - std::cos(theta));
   auto const c1 =
-      -factor * 0.5 * b(n + 1, mu - 1) * std::exp(t_complex(0, -phi)) * (1 + std::cos(theta));
+      -factor * 0.5 * b(n + 1, mu - 1) * std::exp(Complex(0, -phi)) * (1 + std::cos(theta));
   auto const c2 = -factor * a(n, mu) * std::sin(theta);
   auto const coeffs0 = coefficients(n + 1, m - 1, mu + 1);
   auto const coeffs1 = coefficients(n + 1, m - 1, mu - 1);
   auto const coeffs2 = coefficients(n + 1, m - 1, mu);
   std::transform(coeffs0.begin(), coeffs0.end(), std::back_inserter(coeffs),
-                 [c0](t_complex const &coeff) -> t_complex { return c0 * coeff; });
+                 [c0](Complex const &coeff) -> Complex { return c0 * coeff; });
   std::transform(coeffs1.begin(), coeffs1.end(), std::back_inserter(coeffs),
-                 [c1](t_complex const &coeff) { return c1 * coeff; });
+                 [c1](Complex const &coeff) { return c1 * coeff; });
   std::transform(coeffs2.begin(), coeffs2.end(), std::back_inserter(coeffs),
-                 [c2](t_complex const &coeff) { return c2 * coeff; });
+                 [c2](Complex const &coeff) { return c2 * coeff; });
 }
 
-t_complex RotationCoefficients::recursion(t_uint n, t_int m, t_int mu) {
+RotationCoefficients::Complex RotationCoefficients::recursion(t_uint n, t_int m, t_int mu) {
   assert(n >= 1);
-  auto const factor = std::exp(t_complex(0, chi)) / b(n + 1, m - 1);
+  auto const factor = std::exp(Complex(0, chi)) / b(n + 1, m - 1);
   auto const c0 =
-      factor * 0.5 * b(n + 1, -mu - 1) * std::exp(t_complex(0, phi)) * (1 - std::cos(theta));
+      factor * 0.5 * b(n + 1, -mu - 1) * std::exp(Complex(0, phi)) * (1 - std::cos(theta));
   auto const c1 =
-      -factor * 0.5 * b(n + 1, mu - 1) * std::exp(t_complex(0, -phi)) * (1 + std::cos(theta));
+      -factor * 0.5 * b(n + 1, mu - 1) * std::exp(Complex(0, -phi)) * (1 + std::cos(theta));
   auto const c2 = -factor * a(n, mu) * std::sin(theta);
   return c0 * operator()(n + 1, m - 1, mu + 1) + c1 * operator()(n + 1, m - 1, mu - 1) +
          c2 * operator()(n + 1, m - 1, mu);
--- RotationCoefficients.h
+++ RotationCoefficients.h
@@ -14,54 +14,57 @@ namespace optimet {
 //! 25, issue 4 pages 1344-1381 (2004), DOI: 10.1137/S1064827501399705.
 class RotationCoefficients {
 public:
+  typedef t_complex Complex;
+  typedef Complex::value_type Real;
   typedef std::tuple<t_uint, t_int, t_int> Index;
   //! Rotation angle in rad
-  t_real const theta;
+  Real const theta;
   //! Rotation angle in rad
-  t_real const phi;
+  Real const phi;
   //! Rotation angle in rad
-  t_real const chi;
+  Real const chi;
 
-  RotationCoefficients(t_real const &theta, t_real const &phi, t_real const &chi)
+  RotationCoefficients(Real const &theta, Real const &phi, Real const &chi)
       : theta(theta), phi(phi), chi(chi) {}
 
   //! \brief Spherical Harmonic Y^m_n projected onto Y^\mu_n
   //! \details from Y^m_n = \sum_\mu T_n^{\mu,n}Y^\mu_n. This operator gives T_n^{\mu, n}.
-  t_complex operator()(t_uint n, t_int m, t_int mu);
+  Complex operator()(t_uint n, t_int m, t_int mu);
   //! \brief Spherical Harmonic Y^m_n projected onto Y^\mu_n
   //! \details from Y^m_n = \sum_\mu T_n^{\mu,n}Y^\mu_n. This operator gives T_n^{\mu, n}.
-  void coefficients(t_uint n, t_int m, t_int mu, std::vector<t_complex> &coeffs);
+  void coefficients(t_uint n, t_int m, t_int mu, std::vector<Complex> &coeffs);
   //! \brief Spherical Harmonic Y^m_n projected onto Y^\mu_n
   //! \details from Y^m_n = \sum_\mu T_n^{\mu,n}Y^\mu_n. This operator gives T_n^{\mu, n}.
-  std::vector<t_complex> coefficients(t_uint n, t_int m, t_int mu) {
-    std::vector<t_complex> coeff;
+  std::vector<Complex> coefficients(t_uint n, t_int m, t_int mu) {
+    std::vector<Complex> coeff;
     coefficients(n, m, mu, coeff);
     return coeff;
   }
   //! \brief Spherical Harmonic Y^m_n projected onto Y^\mu_n
   //! \brief Spherical Harmonic Y^m_n projected onto Y^\mu_n
-  t_complex operator()(Index const &index) {
+  Complex operator()(Index const &index) {
     return operator()(std::get<0>(index), std::get<1>(index), std::get<2>(index));
   }
 
   //! \brief Simplifies access to spherical harmonics
   //! \note There is a (-1)^m factor (Condon-Shortley phase term) missing with respect to the
   //! definition used int the Gumerov paper.
-  t_complex spherical_harmonic(t_uint n, t_int m) const {
+  Complex spherical_harmonic(t_uint n, t_int m) const {
     return boost::math::spherical_harmonic(n, m, theta, phi);
   }
 
 protected:
+  static Real a(t_uint n, t_int m);
+  static Real b(t_uint n, t_int m);
   //! Cache which holds previously computed values
-  std::map<Index, t_complex> cache;
+  std::map<Index, Complex> cache;
   //! Initial values
-  t_complex initial(t_uint n, t_int mu) const {
+  Complex initial(t_uint n, t_int mu) const {
     return (std::abs(mu) % 2 == 0 ? 1 : -1) *
-           std::sqrt(4 * constant::pi / static_cast<t_real>(2 * n + 1)) *
-           spherical_harmonic(n, -mu);
+           std::sqrt(4 * constant::pi / static_cast<Real>(2 * n + 1)) * spherical_harmonic(n, -mu);
   }
   //! Applies recursion
-  t_complex recursion(t_uint n, t_int m, t_int mu);
+  Complex recursion(t_uint n, t_int m, t_int mu);
 };
 }
 #endif
