d7bc369526870ad27257861c5f9668c16071d00d
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-11-11 17:32:36 +0000

    Add c to coefficients

--- Coefficients.h
+++ Coefficients.h
@@ -20,6 +20,13 @@ template <class TYPE = t_real> TYPE b(t_uint n, t_int m) {
   return (m >= 0 ? 1 : -1) * std::sqrt(static_cast<TYPE>((n - m - 1) * (n - m)) /
                                        static_cast<TYPE>((2 * n - 1) * (2 * n + 1)));
 }
+
+template <class TYPE = t_real> TYPE c(t_uint n, t_int m) {
+  if(std::abs(m) > n)
+    return 0;
+  return (m < 0 ? -1 : 1) *
+         std::sqrt((static_cast<t_int>(n) - m) * (static_cast<t_int>(n) + m + 1));
+}
 }
 }
 #endif
