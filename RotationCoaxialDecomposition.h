f556e7665c7cca7aa0163399806878fc31a6ba8a
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-11-15 16:30:40 +0000

    Adding transpose for field translation

--- RotationCoaxialDecomposition.h
+++ RotationCoaxialDecomposition.h
@@ -4,6 +4,7 @@
 #include "Coefficients.h"
 #include "Types.h"
 #include "constants.h"
+#include <iostream>
 
 #include <boost/math/special_functions/spherical_harmonic.hpp>
 
@@ -24,6 +25,7 @@ void rotation_coaxial_decomposition(t_real wavenumber, t_real tz,
   auto const with_n0 = std::abs(std::sqrt(nr) - std::lround(std::sqrt(nr))) <
                        std::abs(std::sqrt(nr + 1) - std::lround(std::sqrt(nr + 1)));
   t_int const N = std::lround(std::sqrt(with_n0 ? nr : nr + 1)) - 1;
+  assert(input.cols() == 2);
   assert((with_n0 and (N + 1) * (N + 1) == input.rows()) or N * (N + 2) == input.rows());
   int const min_n = with_n0 ? 0 : 1;
   auto const index = with_n0 ? [](t_int n, t_int m) { return n * (n + 1) + m; } :
@@ -31,11 +33,11 @@ void rotation_coaxial_decomposition(t_real wavenumber, t_real tz,
   assert(index(min_n, -min_n) == 0);
   assert(index(N, N) + 1 == input.rows());
   out.resize(input.rows(), input.cols());
-  auto const in_phi = [&input, N, index](t_int n, t_int m) -> t_complex {
-    return n > N or std::abs(m) > n ? 0 : input(index(n, m), 0);
+  auto const in_phi = [&input, N, index, min_n](t_int n, t_int m) -> t_complex {
+    return n > N or std::abs(m) > n or n < min_n ? 0 : input(index(n, m), 0);
   };
-  auto const in_psi = [&input, N, index](t_int n, t_int m) -> t_complex {
-    return n > N or std::abs(m) > n ? 0 : input(index(n, m), 1);
+  auto const in_psi = [&input, N, index, min_n](t_int n, t_int m) -> t_complex {
+    return n > N or std::abs(m) > n or n < min_n ? 0 : input(index(n, m), 1);
   };
   auto const out_phi = [&out, index](t_int n, t_int m) -> t_complex & {
     return out(index(n, m), 0);
@@ -47,19 +49,67 @@ void rotation_coaxial_decomposition(t_real wavenumber, t_real tz,
     out.row(0).fill(0);
   // n ≥ 1
   for(t_int n(1); n <= N; ++n) {
+    auto const factor = tz * wavenumber / static_cast<t_real>(n * n + n);
     for(t_int m(-n); m <= n; ++m) {
-      auto const c0 = n * a<t_real>(n, m);
-      auto const c1 = (n + 1) * a<t_real>(n - 1, m);
-      // clang-format off
-      out_phi(n, m) = in_phi(n, m) + tz / static_cast<t_real>(n * n + n) * (
-          t_complex(0, wavenumber * m) * in_psi(n, m)
-          + wavenumber * (c0 * in_phi(n + 1, m) + (n > min_n ? c1 * in_phi(n - 1, m): 0))
-      );
-      out_psi(n, m) = in_psi(n, m) + tz/ static_cast<t_real>(n * n + n) * (
-          t_complex(0, m  * wavenumber) * in_phi(n, m)
-          + wavenumber * (c0 * in_psi(n + 1, m) + (n > min_n ? c1 * in_psi(n - 1, m): 0))
-      );
-      // clang-format on
+      t_complex const cm(0, m * factor);
+      auto const c0 = n * a<t_real>(n, m) * factor;
+      auto const c1 = (n + 1) * a<t_real>(n - 1, m) * factor;
+      out_phi(n, m) =
+          in_phi(n, m) + cm * in_psi(n, m) + c0 * in_phi(n + 1, m) + c1 * in_phi(n - 1, m);
+      out_psi(n, m) =
+          in_psi(n, m) + cm * in_phi(n, m) + c0 * in_psi(n + 1, m) + c1 * in_psi(n - 1, m);
+    }
+  }
+}
+
+//! \brief Tranpose of the rotation-coaxial decomposition of the vector potentials
+//! \param[in] wavenumber
+//! \param[in] tz: translation alongst the z axis
+//! \param[in] input: input vector potential as a 2-column matrix (Φ, Ψ) with spherical harmonic
+//!                   coefficients from order 1 to n. The maximum order is determined from the
+//!                   number of rows in the matrix.
+//| \param[out] out: output vector potential
+template <class T0, class T1>
+void rotation_coaxial_decomposition_transpose(t_real wavenumber, t_real tz,
+                                              Eigen::MatrixBase<T0> const &input,
+                                              Eigen::MatrixBase<T1> &out) {
+  using coefficient::a;
+  auto const nr = input.rows();
+  auto const with_n0 = std::abs(std::sqrt(nr) - std::lround(std::sqrt(nr))) <
+                       std::abs(std::sqrt(nr + 1) - std::lround(std::sqrt(nr + 1)));
+  t_int const N = std::lround(std::sqrt(with_n0 ? nr : nr + 1)) - 1;
+  assert((with_n0 and (N + 1) * (N + 1) == input.rows()) or N * (N + 2) == input.rows());
+  int const min_n = with_n0 ? 0 : 1;
+  auto const index = with_n0 ? [](t_int n, t_int m) { return n * (n + 1) + m; } :
+                               [](t_int n, t_int m) { return n * (n + 1) + m - 1; };
+  assert(index(min_n, -min_n) == 0);
+  assert(index(N, N) + 1 == input.rows());
+  out.resize(input.rows(), input.cols());
+  auto const in_phi = [&input, N, index, min_n](t_int n, t_int m) -> t_complex {
+    return n > N or std::abs(m) > n or n < min_n ? 0 : input(index(n, m), 0);
+  };
+  auto const in_psi = [&input, N, index, min_n](t_int n, t_int m) -> t_complex {
+    return n > N or std::abs(m) > n or n < min_n ? 0 : input(index(n, m), 1);
+  };
+  auto const out_phi = [&out, index](t_int n, t_int m) -> t_complex & {
+    return out(index(n, m), 0);
+  };
+  auto const out_psi = [&out, index](t_int n, t_int m) -> t_complex & {
+    return out(index(n, m), 1);
+  };
+  if(with_n0)
+    out.row(0).fill(0);
+  // n ≥ 1
+  for(t_int n(1); n <= N; ++n) {
+    auto const factor = tz * wavenumber / static_cast<t_real>(n * n + n);
+    for(t_int m(-n); m <= n; ++m) {
+      t_complex const cm(0, m * factor);
+      auto const c0 = (n + 1) * a<t_real>(n - 1, m) * factor;
+      auto const c1 = n * a<t_real>(n, m) * factor;
+      out_phi(n, m) =
+          in_phi(n, m) + cm * in_psi(n, m) + c0 * in_phi(n - 1, m) + c1 * in_phi(n + 1, m);
+      out_psi(n, m) =
+          in_psi(n, m) + cm * in_phi(n, m) + c0 * in_psi(n - 1, m) + c1 * in_psi(n + 1, m);
     }
   }
 }
@@ -70,6 +120,16 @@ Matrix<typename T0::Scalar>
 rotation_coaxial_decomposition(t_real wavenumber, t_real tz, Eigen::MatrixBase<T0> const &input) {
   Matrix<typename T0::Scalar> out = Matrix<typename T0::Scalar>::Zero(input.rows(), input.cols());
   rotation_coaxial_decomposition(wavenumber, tz, input, out);
+  return out;
+}
+
+//! Computes rotation-coaxial decomposition transpose and returns result
+template <class T0>
+Matrix<typename T0::Scalar>
+rotation_coaxial_decomposition_transpose(t_real wavenumber, t_real tz,
+                                         Eigen::MatrixBase<T0> const &input) {
+  Matrix<typename T0::Scalar> out = Matrix<typename T0::Scalar>::Zero(input.rows(), input.cols());
+  rotation_coaxial_decomposition_transpose(wavenumber, tz, input, out);
   return out;
 }
 }
--- unittests/rotation_coaxial_decomposition.cpp
+++ unittests/rotation_coaxial_decomposition.cpp
@@ -1,7 +1,7 @@
 #include "catch.hpp"
 
-#include "RotationCoaxialDecomposition.h"
 #include "Coefficients.h"
+#include "RotationCoaxialDecomposition.h"
 #include "Types.h"
 #include "constants.h"
 
@@ -17,12 +17,12 @@ TEST_CASE("Brute check") {
       input(0, 0) = 2;
       input(0, 1) = t_complex(0, 1);
 
-      for(auto const k: {1.0, 1.5}) {
+      for(auto const k : {1.0, 1.5}) {
         auto out = rotation_coaxial_decomposition(k, 2, input);
-        CHECK(out(0, 0).real() == Approx(2 + k * k));
+        CHECK(out(0, 0).real() == Approx(2 + k));
         CHECK(out(0, 0).imag() == Approx(0));
         CHECK(out(0, 1).real() == Approx(0));
-        CHECK(out(0, 1).imag() == Approx(-1));
+        CHECK(out(0, 1).imag() == Approx(1 - 2 * k));
       }
     }
     SECTION("n=2") {
@@ -52,7 +52,7 @@ TEST_CASE("Brute check") {
     input(4, 0) = 2;
     input(4, 1) = t_complex(0, 1);
 
-    for(auto const k: {1.0, 1.5}) {
+    for(auto const k : {1.0, 1.5}) {
       auto out = rotation_coaxial_decomposition(k, 2, input);
       CHECK(out(0, 0).real() == Approx(a<>(1, -1) * 2 * k));
       CHECK(out(0, 0).imag() == Approx(0));
@@ -60,4 +60,37 @@ TEST_CASE("Brute check") {
       CHECK(out(0, 1).imag() == Approx(a<>(1, -1) * k));
     }
   }
+}
+
+TEST_CASE("Transpose operation") {
+  auto const wavenumber = 10;
+  auto const tz = 7;
+  auto const N = 10;
+  auto const size = N * (N + 2);
+
+  Matrix<t_complex> actual(2 * size, 2 * size), expected(2 * size, 2 * size);
+  for(t_int i(0); i < size; ++i) {
+    Matrix<t_complex> input = Matrix<t_complex>::Zero(size, 2);
+    input.col(0) = Vector<t_complex>::Unit(size, i);
+
+    auto const phi = rotation_coaxial_decomposition(wavenumber, tz, input);
+    expected.col(i).head(size) = phi.col(0);
+    expected.col(i).tail(size) = phi.col(1);
+
+    auto const phiT = rotation_coaxial_decomposition_transpose(wavenumber, tz, input);
+    actual.col(i).head(size) = phiT.col(0);
+    actual.col(i).tail(size) = phiT.col(1);
+
+    input.col(1) = Vector<t_complex>::Unit(size, i);
+    input.col(0) = Vector<t_complex>::Zero(size);
+
+    auto const psi = rotation_coaxial_decomposition(wavenumber, tz, input);
+    expected.col(i + size).head(size) = psi.col(0);
+    expected.col(i + size).tail(size) = psi.col(1);
+
+    auto const psiT = rotation_coaxial_decomposition_transpose(wavenumber, tz, input);
+    actual.col(i + size).head(size) = psiT.col(0);
+    actual.col(i + size).tail(size) = psiT.col(1);
+  }
+  CHECK(actual.transpose().isApprox(expected));
 }
