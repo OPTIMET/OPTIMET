ea23f9d8d336221d387d110423101ab49ef8b671
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-12-15 16:53:47 +0000

    More backward compatible std::is_fundamental
    I guess I've been pulling in post c++11 stuff because SEINAF is not
    happening. Instead, impossible code is getting compiled.

--- mpi/RegisteredTypes.h
+++ mpi/RegisteredTypes.h
@@ -1,9 +1,9 @@
 #ifndef OPTIMET_MPI_TYPES_H
 #define OPTIMET_MPI_TYPES_H
 
-#include <mpi.h>
-#include <complex>
 #include "Types.h"
+#include <complex>
+#include <mpi.h>
 
 namespace optimet {
 namespace mpi {
@@ -12,48 +12,42 @@ typedef decltype(MPI_CHAR) MPIType;
 
 //! MPI type associated with a c++ type
 template <class T> struct Type;
+//! True if the type is registered
+template <class T> class is_registered_type : public std::false_type {};
 
 static_assert(not std::is_same<char, std::int8_t>::value, "");
-#define OPTIMET_MACRO(TYPE) \
-    template <> struct Type<TYPE> { static const MPIType value; };
-  OPTIMET_MACRO(std::int8_t);
-  OPTIMET_MACRO(std::int16_t);
-  OPTIMET_MACRO(std::int32_t);
-  OPTIMET_MACRO(std::int64_t);
-  OPTIMET_MACRO(std::uint8_t);
-  OPTIMET_MACRO(std::uint16_t);
-  OPTIMET_MACRO(std::uint32_t);
-  OPTIMET_MACRO(std::uint64_t);
+#define OPTIMET_MACRO(TYPE)                                                                        \
+  template <> struct Type<TYPE> { static const MPIType value; };                                   \
+  template <> class is_registered_type<TYPE> : public std::true_type {};
+OPTIMET_MACRO(std::int8_t);
+OPTIMET_MACRO(std::int16_t);
+OPTIMET_MACRO(std::int32_t);
+OPTIMET_MACRO(std::int64_t);
+OPTIMET_MACRO(std::uint8_t);
+OPTIMET_MACRO(std::uint16_t);
+OPTIMET_MACRO(std::uint32_t);
+OPTIMET_MACRO(std::uint64_t);
 
 #ifndef OPTIMET_CHAR_ARCH
-  OPTIMET_MACRO(char);
+OPTIMET_MACRO(char);
 #endif
 #ifndef OPTIMET_LONG_ARCH
-  OPTIMET_MACRO(signed long);
+OPTIMET_MACRO(signed long);
 #endif
 #ifndef OPTIMET_ULONG_ARCH
-  OPTIMET_MACRO(unsigned long);
+OPTIMET_MACRO(unsigned long);
 #endif
 
-  OPTIMET_MACRO(float);
-  OPTIMET_MACRO(double);
-  OPTIMET_MACRO(long double);
-  OPTIMET_MACRO(std::complex<float>);
-  OPTIMET_MACRO(std::complex<double>);
-  OPTIMET_MACRO(std::complex<long double>);
+OPTIMET_MACRO(float);
+OPTIMET_MACRO(double);
+OPTIMET_MACRO(long double);
+OPTIMET_MACRO(std::complex<float>);
+OPTIMET_MACRO(std::complex<double>);
+OPTIMET_MACRO(std::complex<long double>);
 #undef OPTIMET_MACRO
 
 //! MPI type associated with a c++ type
-template <class T> inline MPIType registered_type(T const&) { return Type<T>::value; }
-
-namespace details {
-//! Defines c++17 metafunction
-template <class...> using void_t = void;
-}
-//! True if the type is registered
-template <class T, class = details::void_t<>> class is_registered_type : public std::false_type {};
-template <class T>
-class is_registered_type<T, details::void_t<decltype(Type<T>::value)>> : public std::true_type {};
+template <class T> inline MPIType registered_type(T const &) { return Type<T>::value; }
 
 static_assert(is_registered_type<int>::value, "Checking int is registered");
 static_assert(is_registered_type<std::complex<double>>::value,
